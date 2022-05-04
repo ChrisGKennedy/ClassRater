607251a91301c37398a2ec2132908ea00ada393c
ggreiner333 <ggreiner@trinity.edu>
2022-05-04 10:48:19 -0500

    a few minor changes

--- client/src/components/AdminHomePage.js
+++ client/src/components/AdminHomePage.js
@@ -3,7 +3,7 @@ import React, { Fragment } from "react";
 const AdminHomePage = () => {
     return (
         <div>
-          <h1 className = "text-center mt-5">Home</h1>
+          <h1 className = "text-center mt-5">Admin Home</h1>
           <div className = "text-center mt-5">This is the admin home page.</div>
         </div>
     );
