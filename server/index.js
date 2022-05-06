12bcfb3173b811ea30da654757fd2cceb1c1ed98
Christian Kennedy <christiangkennedy@gmail.com>
2022-05-06 05:10:50 -0500

    added heroku to safe websites morer

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const pool = require("./db");
 const port = process.env.PORT || 5000
 
 const corsOptions = {
-    origin: ["*"],
+    origin: ["classraterserver.herokuapp.com", "classrater.herokuapp.com"],
     credentials: true
 }
 app.use(cors(corsOptions));
