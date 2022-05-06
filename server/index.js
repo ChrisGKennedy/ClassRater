e55d1af7eacf2b0b801c40579db2479a70fc3d0e
Christian Kennedy <christiangkennedy@gmail.com>
2022-05-06 04:40:54 -0500

    added heroku to safe websites

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const pool = require("./db");
 const port = process.env.PORT || 5000
 
 const corsOptions = {
-    origin: ["http://classraterserver.herokuapp.com", 'localhost:5000'],
+    origin: ["http://classraterserver.herokuapp.com", "http://classrater.herokuapp.com"],
     credentials: true
 }
 app.use(cors(corsOptions));
