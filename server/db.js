1dc60489632c1fb5fe83f5fc6ea30317860850d2
Christian Kennedy <christiangkennedy@gmail.com>
2022-05-06 04:07:39 -0500

    gettting deployment working

--- server/db.js
+++ server/db.js
@@ -1,11 +1,19 @@
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "webapp",
+//     host: "localhost",
+//     port: 5432,
+//     database: "cosb"
+// });
+
 const pool = new Pool({
-    user: "postgres",
-    password: "webapp",
-    host: "localhost",
-    port: 5432,
-    database: "cosb"
+    max: 10,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
